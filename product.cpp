49d8605599082ca59318683843c47cc0d3dd15c4
Shailaja Choudhary <81804527+pseudo-me-shailaja@users.noreply.github.com>
2021-05-14 20:37:34 +0530

    product of two numbers
    program to write product of two numbers

---
+++ product.cpp
@@ -0,0 +1,15 @@
+# ASSIGNMENT
+Developes by Shailaja Choudhary
+
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+  float a,b,pro;
+  cout<<"Enter two numbers:";
+  cin>>a>>b;
+  pro=a*b;
+  cout<<"\n Product = "<<pro;
+  return 0;
+}
